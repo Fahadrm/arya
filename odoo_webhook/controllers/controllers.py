67ed2cb13ffed68c513c8ee64b2b327662526654
ar5594ar <aryair100@gmail.com>
2021-08-30 12:44:38 +0530

    Add files via upload

--- odoo_webhook/controllers/controllers.py
+++ odoo_webhook/controllers/controllers.py
@@ -7,9 +7,10 @@ _logger = logging.getLogger(__name__)
 
 
 class WebhookController(http.Controller):
-    @http.route('/webhook/products/<string:auth_token>', type='json', auth='public', methods=['POST'])
-    def webhook(self, auth_token, **post):
-        _logger.info('RESPONSE RECEIVED FROM YELO IS %r', auth_token)
+    @http.route('/webhook/products', type='json', auth='public', methods=['POST'])
+    def webhook(self, **post):
+        header_info = request.httprequest.headers
+        _logger.info('RESPONSE RECEIVED FROM YELO IS %r', header_info)
         # print('post_data', post)
         # yelo_product = request.env["yelo.products"].sudo().search(
         #     [('yelo_order_id', '=', post['order_id'])])
