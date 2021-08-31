c5a5c9358a3a2f2bb8f932183278ed0a1387f1af
ar5594ar <aryair100@gmail.com>
2021-08-31 12:04:08 +0530

    Update controllers.py

--- controllers.py
+++ controllers.py
@@ -10,7 +10,7 @@ class WebhookController(http.Controller):
     @http.route('/webhook/products', type='json', auth='public', methods=['POST'])
     def webhook(self, **post):
         header_info = request.httprequest.headers
-        _logger.info('RESPONSE RECEIVED FROM YELO IS %r', header_info.environ)
+        _logger.info('RESPONSE RECEIVED FROM YELO IS %r', header_info.environ.get('HTTP_X_YELO_TOKEN'))
         # print('post_data', post)
         # yelo_product = request.env["yelo.products"].sudo().search(
         #     [('yelo_order_id', '=', post['order_id'])])
