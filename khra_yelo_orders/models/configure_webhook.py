fcc225c8979154c8407712047208bc3c1c02abd0
Fahadrm <55850573+Fahadrm@users.noreply.github.com>
2021-09-10 16:57:12 +0530

    Add files via upload

--- khra_yelo_orders/models/configure_webhook.py
+++ khra_yelo_orders/models/configure_webhook.py
@@ -8,6 +8,8 @@ class ConfigureWebhook(models.Model):
     _description = 'Webhook Configuration'
 
     name = fields.Char(required=True, string='Name')
-    webhook_auth_key = fields.Char(string="Auth Key")
-    request_url = fields.Char(string="URL", required=True)
-    api_key = fields.Char(string="Api Key", required=True)
+    webhook_auth_key = fields.Char(string="Yelo Webhook Auth Key")
+    request_url = fields.Char(string="Yelo URL", required=True, default="https://api.yelo.red/open/orders/getDetails")
+    api_key = fields.Char(string="Yelo Api Key", required=True)
+    tookan_agent_profile_url = fields.Char(string="Tookan View Agent Profile URL", required=True, default="https://api.tookanapp.com/v2/view_fleet_profile")
+    tookan_api_key = fields.Char(string="Tookan Api Key", required=True)
