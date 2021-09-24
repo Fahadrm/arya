b5fc95d92baf634d619523f520ea742792605457
ar5594ar <aryair100@gmail.com>
2021-09-24 11:29:34 +0530

    Add files via upload

--- khra_account_settings/models/models.py
+++ khra_account_settings/models/models.py
@@ -14,12 +14,12 @@ class ResPartner(models.Model):
     yelo_customer_id = fields.Integer(string='Customer ID')
     yelo_restaurant_id = fields.Integer(string='Restaurant ID')
     yelo_delivery_boy_id = fields.Integer(string='Delivery Boy ID')
-    # restaurant_type = fields.Selection(selection=[
-    #     ('silver', 'Silver'),
-    #     ('gold', 'Gold'),
-    #     ('platinum', 'Platinum'),
-    # ], string="Restaurant Type", default='silver')
-    # restaurant_commission = fields.Float(string="Commission", digits='Product Price')
+    restaurant_type = fields.Selection(selection=[
+        ('silver', 'Silver'),
+        ('gold', 'Gold'),
+        ('platinum', 'Platinum'),
+    ], string="Restaurant Type", default='silver')
+    restaurant_commission = fields.Float(string="Commission", digits='Product Price')
     # account_properties_partner_type = fields.Boolean('Setting properties',
     #                                                  # compute='_compute_property_values'
     #                                                  )
